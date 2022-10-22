84f460db1eac4776fe70a5863be764904ccef0d0
medbendadi <itsbendadi@gmail.com>
2022-10-22 20:24:42 +0100

    adding revalidate to fetch data without needing to rebuild the entire site

--- pages/projects.js
+++ pages/projects.js
@@ -34,6 +34,7 @@ export const getStaticProps = async (context) => {
    const url = `${process.env.SANITY_URL}query=${query}`;
    const data = await fetch(url).then((res) => res.json());
    const projects = data.result;
+   console.log(projects);
    if (!projects || !projects.length === 0) {
       return {
          props: {
