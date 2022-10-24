b2d0be51f0544d7fd4435faf9c25f6c27e9d5af1
medbendadi <itsbendadi@gmail.com>
2022-10-24 11:52:35 +0100

    edit seo options

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: true
 }
 const images = {
   domains: ['cdn.sanity.io', "avatars.githubusercontent.com"],
