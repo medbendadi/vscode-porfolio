c23c4323eba8e87abf2004b77f3aa764307ba1d7
medbendadi <itsbendadi@gmail.com>
2023-11-21 11:48:19 +0100

    Edit Meta tags

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -18,8 +18,8 @@ const CustomHead = ({ title }) => {
             property="og:description"
             content="A full-stack developer building websites that you'd like to use."
          />
-         <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-         <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+         <meta property="og:image" content="https://i.imgur.com/LxdmZQd.png" />
+         <meta property="og:url" content="https://medbendadi.vercel.app" />
          <meta name="twitter:card" content="summary_large_image" />
       </Head>
    );
